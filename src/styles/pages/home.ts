28d6c19cac35c878097fffbdfb4edcc7ee9b829f
Davi Holanda <daviht7@gmail.com>
2023-01-31 21:13:29 -0300

    criando carossel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,7 +3,7 @@ import { styled } from "..";
 export const HomeContainer = styled('main', {
   
   display: 'flex',
-  gap: '3rem',
+  //gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
@@ -15,10 +15,11 @@ export const Product = styled('a', {
 
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d3 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
+  //padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
+  minWidth: 540,
 
   display: 'flex',
   alignItems: 'center',
